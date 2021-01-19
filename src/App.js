384baa89662588db67c449e6d2029050c1049fc3
SWi74 <dum66a@gmail.com>
2021-01-19 14:14:08 +0200

    Start

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import React from "react"
 import Theme from "./Theme"
 import GlobalStyle from "./GlobalStyle"
 
-import Header from "./components/header/header"
 import Main from "./components/main/main"
-import Footer from "./components/footer/footer"
 
 const App = () => (
   <Theme>
     <GlobalStyle />
-    <Header />
     <Main />
-    <Footer />
   </Theme>
 )
 
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,29 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Switch, Route } from "react-router-dom"
-
-import Add from "./home/add"
-import Device from "./home/device"
-import FromBP from "./home/fromBP50"
-
 const StyledMain = styled.main`
   margin: 0 0 auto 0;
 `
 
 const Main = () => (
   <StyledMain>
-    <Switch>
-      <Route exact path="/">
-        <Device />
-      </Route>
-      <Route exact path="/add">
-        <Add />
-      </Route>
-      <Route exact path="/fromBP">
-        <FromBP />
-      </Route>
-    </Switch>
+    <h1>asdasdasd</h1>
   </StyledMain>
 )
 
